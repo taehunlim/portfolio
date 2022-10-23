4fc4656bddee162c9d547c14fe21c1b8bce38781
taehunlim <dsa1520@naver.com>
2022-10-23 12:48:30 +0900

    Refactor: move index page content to IndexTemplate

--- src/components/templates/IndexTemplate/style.tsx
+++ src/components/templates/IndexTemplate/style.tsx
@@ -1,13 +1,28 @@
 import styled from '@emotion/styled';
 
-const Wrapper = styled.div`
+import ScrollAnimation from 'components/organisms/ScrollAnimation';
+
+const Container = styled.div`
    height: 100%;
    position: relative;
    z-index: 8;
 `;
 
+const SlideContainer = ScrollAnimation;
+
+const StyledSlide = styled(ScrollAnimation.Slide)`
+   width: 40%;
+   min-width: 320px;
+
+   a {
+      color: ${({ theme }) => theme.fg.white};
+   }
+`;
+
 const styledComponent = {
-   Wrapper,
+   Container,
+   SlideContainer,
+   StyledSlide,
 };
 
 export default styledComponent;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from '@emotion/styled';
 
 import IndexTemplate from 'components/templates/IndexTemplate';
 
-import ScrollAnimation from 'components/organisms/ScrollAnimation';
-
 function Index() {
-   return (
-      <IndexTemplate>
-         <ScrollAnimation>
-            <StyledSlide>
-               <hgroup>
-                  <h1>개발자가 상발자</h1>
-                  <h2>THE ODYSSEY OF A FRONT WEB DEVELOPER</h2>
-               </hgroup>
-
-               <div>image</div>
-               <p>3년차 개발자의 삽질 여정기</p>
-               <p>오늘도 삽질하며 깊이를 판다</p>
-            </StyledSlide>
-            <StyledSlide>
-               <hgroup>
-                  <h1>💪 Technical Skills</h1>
-               </hgroup>
-               <p>vanilaJS, React, typescript</p>
-               <p>nodeJS, espressJS, mongoDB, postman</p>
-               <p>sass,styled-component, emotion-component</p>
-               <p>bootstrap, antd</p>
-               <p>Redux, recoil</p>
-               <p>useSWR, react-query</p>
-               <p>webpack, babel, vite</p>
-               <p>jest</p>
-            </StyledSlide>
-            <StyledSlide>
-               <hgroup>
-                  <h1>💻 경력</h1>
-               </hgroup>
-               <p>2020 ~ 2021 프리랜서</p>
-               <p>2021 ~ 2022 피프틴에이치</p>
-               <p>2022 ~ 현재 웹웨어</p>
-            </StyledSlide>
-            <StyledSlide>
-               <hgroup>
-                  <h1>Contacts</h1>
-               </hgroup>
-               <p>📞 +82 </p>
-               <p>✉️ naver.com</p>
-            </StyledSlide>
-            <StyledSlide>
-               <Link to="/about">🙋🏻‍♂️ ABOUT ME</Link>
-            </StyledSlide>
-         </ScrollAnimation>
-      </IndexTemplate>
-   );
+   return <IndexTemplate />;
 }
-
-const StyledSlide = styled(ScrollAnimation.Slide)`
-   width: 40%;
-   min-width: 320px;
-`;
 
 export default Index;
